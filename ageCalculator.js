62a1b3e18cb1408c10bff14a17edbaf18e57e945
Osman Büyükçerçi <121210041+OsmanBuyukcerci@users.noreply.github.com>
2023-02-07 21:23:17 +0000

    version 0.11

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,6 +1,6 @@
-
 document.getElementById("button").onclick = function calc() {
     var yearOfBirth = parseInt(document.getElementById("yearofbirth").value);
     var age = new Date().getFullYear() - yearOfBirth;
-    document.getElementById("age").value = age;
+    var result = getElementById("result");
+    result = "Your age is "+age;
 };
