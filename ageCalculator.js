2ad1c5dc0d5dafab3e976f0269dfe5fb4c9ce5ad
Osman Büyükçerçi <121210041+OsmanBuyukcerci@users.noreply.github.com>
2023-02-08 11:23:51 +0000

    ver 0.12

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,4 +1,21 @@
+// ver 0.12
+
 document.getElementById("button").onclick = function calc() {
-    var age = new Date().getFullYear() - document.getElementById("yearofbirth").value;
-    document.getElementById("resultMsg").innerHTML = "Your age is "+age;
+    var year = document.getElementById("yearInput").value;
+    var month = document.getElementById("monthInput").value;
+    var date = document.getElementById("dateInput").value;
+    var birthday = new Date();
+    birthday.setFullYear(year);
+    birthday.setMonth(month-1);
+    birthday.setDate(date);
+    var ageMilisecond = new Date() - birthday;
+    ageSecond = ageMilisecond / 1000;
+    ageMinute = ageSecond / 60;
+    ageHour = ageMinute / 60;
+    ageDay = ageHour / 24;
+    ageYear = ageDay / 365;
+
+    document.getElementById("yearOutput").innerHTML = "Your age is " + parseInt(ageYear);
+    document.getElementById("hourOutput").innerHTML = "You lived for " +ageHour + " hours";
+    
 }
